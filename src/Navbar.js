8465a32f6fd2ab36d2ae8e246bd0c90bb4b88773
Mansi More <moremansi0817@gmail.com>
2025-10-18 14:16:27 +0530

    Refactor footer and navbar components, enhance Open Account page, and update CSS styles for improved layout and user experience

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,9 +1,62 @@
-import React from 'react';
+import React from "react";
 
 function Navbar() {
-    return ( 
-        <h1>Navbar</h1>
-     );
+  return (
+   
+      <nav class="navbar navbar-expand-lg border-bottom p-3"  style={{backgroundColor: "#ffff"}}>
+        <div class="container-fluid ">
+          <a class="navbar-brand" href="#">
+            <img src="images/logo.svg" alt= "logo" style={{width: "25%"}}/>
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+            
+            <form class="d-flex " role="search">
+                <ul class="navbar-nav mb-2 mb-lg-0" style={{ paddingRight: "40px" }}>
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">
+                    Sign Up
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" href="#">
+                    About
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">
+                    Product
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" href="#">
+                    Pricing
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" href="#">
+                    Support
+                </a>
+              </li>
+              
+                
+            </ul>
+            </form>
+          </div>
+        </div>
+      </nav>
+  
+  );
 }
 
 export default Navbar;
--- src/OpenAccount.js
+++ src/OpenAccount.js
@@ -2,7 +2,15 @@ import React from 'react';
 
 function OpenAccount() {
     return ( 
-        <h1>Open Account</h1>
+        <div className="container p-5" >
+      <div className="row text-center">
+        <h1 className="mt-5">Open Account in EquiTrade</h1>
+        <p>
+            Join millions of investors and traders who trust EquiTrade for their trading and investment needs.
+        </p>
+        <button className="p-3 btn btn-primary fs-5 mb-5" style={{width: "20%", margin: "0 auto"}}>Sign Up Now</button>
+      </div>
+    </div>
      );
 }
 
