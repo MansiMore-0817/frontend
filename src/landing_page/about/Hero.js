c64c1682e1992a384c6dcd69b83949c6e699206f
Mansi More <moremansi0817@gmail.com>
2025-11-27 19:56:54 +0530

    bug fixed

--- src/landing_page/about/Hero.js
+++ src/landing_page/about/Hero.js
@@ -15,9 +15,7 @@ function Hero() {
           <p>
             We kick-started operations on the 15th of August, 2010 with the goal
             of breaking all barriers that traders and investors face in India in
-            terms of cost, support, and technology. We named the company
-            Zerodha, a combination of Zero and "Rodha", the Sanskrit word for
-            barrier. <br />
+            terms of cost, support, and technology. <br />
             <br />
             Today, our disruptive pricing models and in-house technology have
             made us the biggest stock broker in India.
